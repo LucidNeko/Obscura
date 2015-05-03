176f69fc1ccb15b8e75ab503440fcf170e3fae67
Hamish <Guest@f-06-06-lab.mddn.vuw.ac.nz>
2015-05-03 18:52:18 +1200

    Pushable blocks

---
+++ Assets/Scripts/BallInstantiator.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallInstantiator : MonoBehaviour {
+
+	public GameObject m_Object;
+	public float m_NumBalls = 100;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (Spawn ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	IEnumerator Spawn() {
+		for(int i = 0; i < m_NumBalls; i++) {
+			Instantiate(m_Object, transform.position, transform.rotation);
+			yield return new WaitForSeconds(0.2f);
+		}
+	}
+}
---
+++ Assets/Scripts/GetShoved.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GetShoved : MonoBehaviour {
+
+	public float m_Force = 10f;
+
+	Rigidbody m_body;
+
+	// Use this for initialization
+	void Start () {
+		m_body = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		if (collision.gameObject.CompareTag ("Player")) {
+			Shove (collision.contacts [0].normal);
+		}
+	}
+
+	void OnCollisionStay(Collision collision) {
+		if (collision.gameObject.CompareTag ("Player")) {
+			Shove (collision.contacts [0].normal);
+		}
+	}
+
+	private void Shove(Vector3 direction) {
+		m_body.AddForce(direction * m_Force, ForceMode.Impulse);
+	}
+}
