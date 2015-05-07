43f750845f5a410a44ca02e33d92e0b2479f12a5
Hamish <Guest@d-05-06-lab.mddn.vuw.ac.nz>
2015-05-07 21:16:31 +1200

    Added light to player in dark world

---
+++ Assets/TrackGameObject.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrackGameObject : MonoBehaviour {
+
+	public Transform m_Target;
+
+	private Vector3 m_Offset;
+
+	void Start() {
+		m_Offset = transform.localPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = m_Offset + m_Target.position;
+	}
+}
