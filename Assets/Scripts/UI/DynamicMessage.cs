fcfc616bf1be5b6d637d3126f1ae900ca0409e8b
Mike Cripps <Guest@f-05-06-lab.mddn.vuw.ac.nz>
2015-05-18 12:45:58 +1200

    Further development of dynamic messaging

---
+++ Assets/Scripts/UI/DynamicMessage.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DynamicMessage : MonoBehaviour {
+
+	public Animator panel;
+	public Text message;
+
+	void Start(){
+		panel.SetBool("isHidden", true);
+	}
+
+
+	void OnCollisionEnter(Collision Collection)
+	{
+		if(Collection.gameObject.name == "Player")
+		{
+			message.text = "I changed the text";
+			panel.SetBool("isHidden", false);
+		}
+	}
+
+}
