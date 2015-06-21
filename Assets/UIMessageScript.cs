75ea1b87428df47d737acdfeb183fb7283bd8384
Guest User <Guest@a-08-08-lab.mddn.vuw.ac.nz>
2015-06-21 23:28:35 +1200

    Editing UI messaging

---
+++ Assets/UIMessageScript.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public enum messageTypes {Camera, Jump, Movement, Spin, Custom};
+
+public class UIMessageScript : MonoBehaviour {
+
+
+	public GameObject panel;
+	public Text message;
+	public messageTypes messageType;
+	public string customMessage;
+	public bool repeat = false;
+
+	Animator animator;
+	bool hasPlayed = false;
+
+	string cameraMessage = "";
+
+
+	// Use this for initialization
+	void Start () {
+		animator = panel.GetComponent<Animator>();
+
+	}
+	
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (!hasPlayed || repeat) {
+			if(other.gameObject.CompareTag("Player"))
+			{
+				displayMessage(messageType);
+				//pushPanel.SetBool("ShiftWorld Message Hidden", false);
+			}
+		}
+	}
+	
+	
+	void OnTriggerExit(Collider other)
+	{
+		if (!hasPlayed || repeat) {
+			if(other.gameObject.CompareTag("Player"))
+			{
+				hasPlayed = true;
+				//pushPanel.SetBool("ShiftWorld Message Hidden", true);
+			}
+		}
+	}
+
+
+
+	public void displayMessage(messageTypes type){
+
+		switch (type) {
+		case messageTypes.Camera:
+			message.text = "Bla bla camera";
+			break;
+
+		case messageTypes.Jump:
+			message.text = "Bla bla jump";
+			break;
+		}
+
+	}
+
+}
